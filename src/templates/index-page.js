913fc9c8774c8c16069d65e8639664ee3cfa3f7a
chriact <113547086+chriact@users.noreply.github.com>
2023-01-23 21:39:49 +0100

    oo

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -23,10 +23,7 @@ export const IndexPageTemplate = ({
   return (
     <div>
       <FullWidthImage img={heroImage} title={title} subheading={subheading} />
-      <div id="widget_62cc00cd0ebef" >... lade FuPa Widget ...
-        <script type="text/javascript">
-          !function(i){window.setTimeout(function(){"undefined"===typeof fupa_widget_domain?--i.t>0&&window.setTimeout(arguments.callee,i.i):i.f()},i.i)}({i:20,t:100,f:function(){team_widget(1034553,{act:"liveticker",hea:0,nav:0,div:"widget_62cc00cd0ebef",mod:"0"})}});</script>
-      </div>
+
       <section className="section section--gradient">
         <div className="container">
           <div className="section">
